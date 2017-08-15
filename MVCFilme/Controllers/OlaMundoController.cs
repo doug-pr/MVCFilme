064fc3cb753f1d344e954288c6e47b00aa6f9807
DoUg Alves <doug-pr@hotmail.com>
2017-08-15 00:13:34 -0300

    Init

---
+++ MVCFilme/Controllers/OlaMundoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCFilme.Controllers
+{
+    public class OlaMundoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string BemVindo(string nome, int numVezes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Ola {nome}, o numero de vezes e igual a: {numVezes}.");
+        }
+    }
+}
