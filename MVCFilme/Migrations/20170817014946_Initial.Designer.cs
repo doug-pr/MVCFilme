ab7d34a7ef5a89eb2a20c13f934e395c472111ac
DoUg Alves <doug-pr@hotmail.com>
2017-08-16 23:10:19 -0300

    Gerado o Banco com EF

---
+++ MVCFilme/Migrations/20170817014946_Initial.Designer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MVCFilme.Models;
+
+namespace MVCFilme.Migrations
+{
+    [DbContext(typeof(MVCFilmeContext))]
+    [Migration("20170817014946_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MVCFilme.Models.Filme", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genero");
+
+                    b.Property<DateTime>("Lancamento");
+
+                    b.Property<decimal>("Preco");
+
+                    b.Property<string>("Titulo");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Filme");
+                });
+        }
+    }
+}
---
+++ MVCFilme/Migrations/MVCFilmeContextModelSnapshot.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MVCFilme.Models;
+
+namespace MVCFilme.Migrations
+{
+    [DbContext(typeof(MVCFilmeContext))]
+    partial class MVCFilmeContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MVCFilme.Models.Filme", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Genero");
+
+                    b.Property<DateTime>("Lancamento");
+
+                    b.Property<decimal>("Preco");
+
+                    b.Property<string>("Titulo");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Filme");
+                });
+        }
+    }
+}
